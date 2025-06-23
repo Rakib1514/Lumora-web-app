dbfef884142b34604d12923eec559d81b48ff883
Rakib1514 <rkrakib1514@outlook.com>
2025-06-23 15:09:03 +0600

    feat: user api added
    - user model, controller, route added
    - user create api created

---
+++ server/src/controllers/userController.js
@@ -0,0 +1,31 @@
+const User = require("../models/userModel");
+
+const createUser = async (req, res) => {
+  try {
+    const userData = req.body;
+    // Validate user data
+    if (!userData.email || !userData.firebaseUid) {
+      return res.status(400).json({ message: "User creation failed" });
+    }
+
+    // Check if user already exists
+    const existingUser = await User.findOne({
+      $or: [{ email: userData.email }, { firebaseUid: userData.firebaseUid }],
+    });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    // Create new user
+    const newUser = await User.create({ userData });
+    res.status(201).json({
+      message: "User created successfully",
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = {
+  createUser,
+};
---
+++ server/src/models/userModel.js
@@ -0,0 +1,39 @@
+const { default: mongoose } = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    firebaseUid: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    image: {
+      type: String,
+      default: "https://example.com/default-avatar.png",
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
