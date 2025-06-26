eb94be6286acd9b284c55b1e7d4f83327cff22d1
Rakib1514 <rkrakib1514@outlook.com>
2025-06-26 22:02:52 +0600

    added middleware for verify token at authMiddleware.js

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -1,1 +1,19 @@
+const jwt = require("jsonwebtoken");
 
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "unauthorize access" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+  } catch (error) {
+    return res.status(403).json({ message: "Verification failed" });
+  }
+};
+
+module.exports = { verifyToken };
