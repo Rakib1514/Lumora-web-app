a7a391f6af7d13cfbd3d043189a4bf1d0249d78d
Rakib1514 <rkrakib1514@outlook.com>
2025-06-27 12:12:23 +0600

    verfiy admin middleware added

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
 const verifyToken = async (req, res, next) => {
   const token = req.cookies?.token;
@@ -9,11 +10,27 @@ const verifyToken = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
     req.user = decoded;
+    next();
   } catch (error) {
     return res.status(403).json({ message: "Verification failed" });
   }
 };
 
-module.exports = { verifyToken };
+const verifyAdmin = async (req, res, next) => {
+  const user = req.user;
+
+  try {
+    const result = await User.findById(user._id);
+
+    if (result.role !== "admin") {
+      return res.status(401).json({
+        message: "unauthorize access",
+      });
+    }
+
+    next();
+  } catch (error) {}
+};
+
+module.exports = { verifyToken, verifyAdmin };
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -3,9 +3,10 @@ const {
   setTokenCookie,
   clearTokenCookie,
 } = require("../controllers/tokenController");
+const { verifyAdmin, verifyToken } = require("../middleware/authMiddleware");
 const router = express.Router();
 
-router.post("/jwt-signin", setTokenCookie);
+router.post("/jwt-signin",verifyToken, verifyAdmin, setTokenCookie);
 router.post("/jwt-signout", clearTokenCookie);
 
 module.exports = router;
